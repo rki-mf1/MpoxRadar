a08124a861c1eb0cb833fe120139772f1dc6dcae
K2 <kunaphas.kon@gmail.com>
2023-05-02 16:52:42 +0200

    update GitAction

--- tests/sql_dumps/mpx_test_04.sql
+++ tests/sql_dumps/mpx_test_04.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `alignment`
 --
 
+CREATE DATABASE IF NOT EXISTS mpx_test_04;
+
 DROP TABLE IF EXISTS `alignment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
