934f86c80cecaafa5ce57f063a7a4879905a0843
Ferdous Nasri <ferbsx@users.noreply.github.com>
2023-08-01 15:15:57 +0200

    Update util_footer_table.py with better bmws logo

--- pages/util_footer_table.py
+++ pages/util_footer_table.py
@@ -44,32 +44,13 @@ row1 = html.Tr(
             className="responsive",
         ),
         html.Td(
-            html.Ul(
-                [
-                    html.Li([html.Div(children="Supported by:")]),
-                    html.Li(
-                        [
-                            html.Img(
-                                src=r"assets/Bundesministerium_für_Wirtschaft_und_Energie_Logo.svg.png",
-                                alt="Img_RKI",
-                                style={
-                                    "marginTop": "-10px",
-                                    "height": "auto",
-                                    "minWidth": "100%",
-                                },
-                                className="responsive",
-                            )
-                        ],
-                    ),
-                    html.Li(
-                        [html.Div(children="on the basis of a decision")],
-                        style={"marginTop": "-20px"},
-                    ),
-                    html.Li([html.Div(children="by the German Bundestag")]),
-                ],
-                style={"listStyleType": "none", "textAlign": "center"},
+            html.Img(
+                src=r"assets/BMWK_Logo_en.png",
+                alt="BMWK_logo",
+                style={"height": "auto", "minWidth": "100%", "marginTop": "10px"},
+                className="responsive",
             ),
-            style={"width": "15%"},
+            style={"textAlign": "left", "width": "15%"},
             className="responsive",
         ),
         html.Td(
