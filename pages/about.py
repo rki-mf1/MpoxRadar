d04822984c663bf84e341e95f9ebface897a426c
Ferdous Nasri <ferbsx@users.noreply.github.com>
2023-08-01 14:18:30 +0200

    Update about.py with published paper link

--- pages/about.py
+++ pages/about.py
@@ -27,7 +27,7 @@ layout = html.Div(
                                         dbc.Col(
                                             [
                                                 """
-                                            MonkeyPox (Mpox) is an infectious disease caused by a smallpox virus, recently spreading in multiple countries with over 86,000 cases and declared a global emergency by the World Health Organization """,
+                                            MonkeyPox (Mpox) is an infectious disease caused by a smallpox virus, recently spreading in multiple countries with over 88,000 cases and declared a global emergency by the World Health Organization """,
                                                 dcc.Link(
                                                     html.A("[1]"),
                                                     href="https://worldhealthorg.shinyapps.io/mpx_global/",
@@ -40,14 +40,14 @@ layout = html.Div(
                                                     target="_blank",
                                                 ),
                                                 """. This alarming behavior demands action and highlights the need for genomic surveillance and spatio-temporal analyses.
-                                                Therefore, the Robert Koch Institute (RKI) together with the Hasso Platter Institute (HPI), joined forces to produce such a dashboard with a strong database background, inspired by their earlier work
+                                                Therefore, the Robert Koch Institute (RKI) together with the Hasso Platter Institute (HPI) joined forces to produce such a dashboard with a strong database background, inspired by the earlier work in our group 
                                                 on """,
                                                 dcc.Link(
                                                     html.A("CovSonar"),
                                                     href="https://github.com/rki-mf1/covsonar",
                                                     target="_blank",
                                                 ),
-                                                """ - a database framework developed at the RKI for SARS-CoV-2. below is a graphical abstract of our work.""",
+                                                """ - a database framework developed at the RKI for SARS-CoV-2. Below is a graphical abstract of our work.""",
                                             ]
                                         ),
                                     ),
@@ -104,9 +104,11 @@ layout = html.Div(
                         [
                             dbc.Row(
                                 dbc.Col(
-                                    """With this web server, we provide tools to explore and comapre metadata from Mpox sequences available from our data sources (listed below).
-                                Furthermore, we provide an advanced tool for more detailed searches. The chosen data using our tools is visualised and presented in downloadable tables.
-                                As Mpox does not have one defined reference genome, we provide multiple reference genomes to choose between. All sample genomes are pre-processed, aligned to multiple reference genomes, followed by variant calling on our servers to enable quick analysis and searches for our users. We confirm that this website is free and open to all users and there is no login requirement. Below is a simplified overview of our tool:
+                                    """With this web server, we provide tools to explore and compare metadata from MPXV sequences available from our data source (listed below).
+                                Furthermore, we provide an advanced tool to enable more detailed queries. The chosen data using our tools is visualised and presented in downloadable tables.
+                                As Mpox does not have one defined reference genome, we provide multiple reference genomes to choose between. All sample genomes are pre-processed, 
+                                aligned to multiple reference genomes, followed by variant calling on our servers to enable quick analysis and searches for our users. 
+                                We confirm that this website is free and open to all users and there is no login requirement. Below is a simplified technical overview of our tool:
                                 """,
                                 ),
                             ),
@@ -157,7 +159,8 @@ layout = html.Div(
                                 dbc.Col(
                                     [
                                         """
-                                    For more information on the tool, we re-direct you to our paper (below under "How to cite?"), and to our GitHub README pages (below under "Link to code").
+                                    For more information on the tool, we re-direct you to our peer-reviewed and published paper (see "How to cite?"), and to our GitHub 
+                                    README pages (see "Link to code").
                                     We have described the functionalities of our tools in detail in the """,
                                         dcc.Link(
                                             html.A("help page"),
@@ -183,6 +186,20 @@ layout = html.Div(
                     html.H2(children="Who are we?"),
                     html.Div(
                         [
+                            dbc.Row(
+                                dbc.Col(
+                                    """In this project, we collaborated between three groups: 
+                                    the Data Analytics & Computational Statistics, Hasso Plattner Institute, University of Potsdam, Germany, and 
+                                    Bioinformatics and Systems Biology, Robert Koch Institute, Berlin, Germany, 
+                                    and Centre for Biological Threats and Special Pathogens, Robert Koch Institute, Berlin, Germany. 
+                                """,
+                                    dcc.Link(
+                                    html.A("Feel free to reach out to us!"),
+                                    href=("Contact"),
+                                    target="_blank",
+                                    ),
+                                ),
+                            ),
                             dbc.Row(
                                 [
                                     dbc.Col(
@@ -488,16 +505,17 @@ layout = html.Div(
                     html.Div(
                         [
                             """
-                                MpoxRadar: a worldwide Mpox genomic surveillance dashboard
-                                Ferdous Nasri, Alice Wittig, Kunaphas Kongkitimanon, Jorge Sánchez Cortés, Annika Brinkmann, Andreas Nitsche, Anna-Juliane Schmachtenberg, Bernhard Y. Renard, Stephan Fuchs
-                                bioRxiv 2023.02.03.526935;(""",
+                                Our work has been published and peer-reviewed in the Nucleic Acids Research (web server issue) in 2023. To cite us, please use the following paper:
+                                'Ferdous Nasri and others, MpoxRadar: a worldwide MPXV genomic surveillance dashboard, Nucleic Acids Research, Volume 51, Issue W1, 
+                                5 July 2023, Pages W331–W337, """,
                             dcc.Link(
                                 html.A(
-                                    "doi: https://doi.org/10.1101/2023.02.03.526935 "
+                                    "https://doi.org/10.1093/nar/gkad325"
                                 ),
-                                href="https://www.biorxiv.org/content/10.1101/2023.02.03.526935v1",
+                                href="https://doi.org/10.1093/nar/gkad325",
                                 target="_blank",
                             ),
+                            "'.",
                         ]
                     ),
                     html.Br(),
@@ -505,7 +523,8 @@ layout = html.Div(
                     html.Div(
                         [
                             """
-                                We want to give a big thanks to all our test users, especially in the central German Public Health institute, for giving us their valuable feedback and helping us better our tool. Furthermore, we want to thank the creators of """,
+                                We want to give a big thanks to all our test users, especially in the central German Public Health institute, for giving us their valuable feedback 
+                                and helping us better our tool. Furthermore, we want to thank the creators of """,
                             dcc.Link(
                                 html.A("CovRadar"),
                                 href=("https://doi.org/10.1093/bioinformatics/btac411"),
@@ -517,7 +536,8 @@ layout = html.Div(
                                 href=("https://github.com/rki-mf1/covsonar"),
                                 target="_blank",
                             ),
-                            """ for showing the need for genomic surveillance dashboard and database for SARS-CoV-2, therefore inspiring the initiation of this project. We are always open to feedback and promise a continued support and developement of our tool. """,
+                            """ for showing the need for genomic surveillance dashboard and database for SARS-CoV-2, therefore inspiring the initiation of this project. 
+                            We are always open to feedback and promise a continued support and developement of our tool. """,
                             dcc.Link(
                                 html.A("Don't hesitate to get in touch."),
                                 href=("Contact"),
